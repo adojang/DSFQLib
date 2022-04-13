d35c0e4549870dc0da4f85e384ff5b724ab995e6
adojang <21786275@sun.ac.za>
2022-04-13 09:09:16 +0200

    AND v1.3
    Improvements to the AND gate which include version 1.3, and it's optimized version with a critical margin of 22%

--- Ki2Spice/ki2spice.py
+++ Ki2Spice/ki2spice.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # Import relevant packages
 import argparse
+from cgi import print_arguments
 from collections import Counter
+from lib2to3.pgen2.token import EQUAL
 
 # Main function
 def main():
@@ -50,23 +52,66 @@ def main():
   netlist = netlist.replace("Inductor", "")
   netlist = netlist.replace("Resistor", "")
   netlistlow = str.lower(netlist)
-  node = 1
+  node = 0
+
+
   #If anything appears more than once (exception of jjmit) then assign a number for a node
   for wordx in unique:
-    if cnt[wordx] > 1 and (wordx != 'jjmit'):
+    if cnt[wordx] > 1 and ((wordx != 'jjmit') and (wordx != 'ref') and (wordx != 'pwl(0')):
         search_text   = wordx
         replace_text = str(node)
+
         node+=1
         netlistlow = netlistlow.replace(search_text, replace_text)
         #print(f"Replaced {wordx} with {replace_text}")
   netlistout = netlistlow.replace(".title kicad schematic", "* Back Annotated .cir file from KiCad")
-  file = open('translated_' + 'args.input', 'w')
+
+  #Split each line 
+  splot = netlistout.splitlines( )
+  splot.sort()
+  lines = len(splot)
+  paramlist = ""
+  currentcount = 1
+
+  for x in range (lines):
+    if splot[x].split()[0] != "*" and splot[x].split()[0] != ".end":
+      
+      #Setup list of parameters, and add nice tabs and spacing
+      paramlist = paramlist + ".param \t" + splot[x].split()[0] + "  \t= \n"
+      splot[x] = splot[x].replace(" ", "   \t")
+      splot[x] = splot[x] + " " + splot[x].split()[0]
+    
+      #add area for JJ's
+      if 'jjmit' in splot[x]:
+        splot[x] = splot[x].replace("jjmit ", " jjmit area=")
+
+      #prevent odd behavior from current sources
+      if ("ref) "+ 'i'+ str(currentcount)) in splot[x]:
+
+        splot[x] = splot[x].replace(("ref) "+ 'i'+ str(currentcount)), 'i' + str(currentcount) + ')') 
+        currentcount+=1
+
+
+
+  #Move '.end' to the end
+  splot.append(splot.pop(splot.index(".end")))
+
+  #Join '.param' and the rest
+  netlistout = "\n".join(splot)
+  netlistout = paramlist + "\n\n" + netlistout
+  
+
+
+
+
+
+  file = open('translated_' + args.input, 'w')
   file.write(netlistout)
   file.close()
 
   print("File Written Sucessfully!")
 
-  #print(netnew)
+  #print(netlistout)
 
 if __name__ == '__main__':
   main()
--- mitll_DSFQ_AND/RunJosim.bat
+++ mitll_DSFQ_AND/RunJosim.bat
@@ -1,7 +1,7 @@
 @ECHO ON
 
 josim -o AW_AND_testbench.csv AW_AND_testbench.cir -V 1
-josim-plot.py AW_AND_testbench.csv
+josim-plot.py AW_AND_testbench.csv -t stacked
 
 
 
@@ -9,7 +9,6 @@ josim-plot.py AW_AND_testbench.csv
 
 
 
- -t stacked
 
 pause
 
